dcb90eb64703ce0e9741fbd73e63fa50a62b7cee
Ruslan Ramazanov <ruslanramazanov@Dev-MacBook-Pro.local>
2021-03-27 01:42:52 +0300

    master - screen height, Visible Area and MiniMap sizes adjustment

--- src/constants/sizes.ts
+++ src/constants/sizes.ts
@@ -1,16 +1,19 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, StatusBar } from 'react-native';
 
 const { width, height } = Dimensions.get('screen');
 const { height: windowHeight } = Dimensions.get('window');
 
+const STATUS_BAR_HEIGHT = StatusBar.currentHeight || 0;
+
 const SCREEN_WIDTH = width;
 const SCREEN_HEIGHT = height;
 const SCREEN_RATIO = height / width;
+const SCREEN_HEIGHT_WITHOUT_STATUS_BAR = SCREEN_HEIGHT - STATUS_BAR_HEIGHT;
 
 const PADDING = 15;
 
-const VISIBLE_AREA_WIDTH = width / 4;
-const VISIBLE_AREA_HEIGHT = height / 4;
+const VISIBLE_AREA_WIDTH = SCREEN_WIDTH / 4;
+const VISIBLE_AREA_HEIGHT = SCREEN_HEIGHT_WITHOUT_STATUS_BAR / 4;
 
 const IMAGE_RATIO = 2.165;
 
@@ -18,7 +21,7 @@ const VIDEO_CIRCLE_SIZE = 30;
 
 const MINI_MAP_RATIO = 4;
 const MINI_MAP_WIDTH = SCREEN_WIDTH / MINI_MAP_RATIO;
-const MINI_MAP_HEIGHT = SCREEN_HEIGHT / MINI_MAP_RATIO;
+const MINI_MAP_HEIGHT = SCREEN_HEIGHT_WITHOUT_STATUS_BAR / MINI_MAP_RATIO;
 const MINI_MAP_LEFT_POSITION = -SCREEN_WIDTH + MINI_MAP_WIDTH + PADDING * 2;
 const MINI_MAP_TOP_POSITION = -windowHeight + MINI_MAP_HEIGHT - PADDING;
 
